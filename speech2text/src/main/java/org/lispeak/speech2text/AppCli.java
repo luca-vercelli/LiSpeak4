565150e259803e8fc53f8d036b3ca80a112b988a
Luca Vercelli <luca.vercelli.to@gmail.com>
2018-03-07 22:22:12 +0100

    at least works

--- speech2text/src/main/java/org/lispeak/speech2text/AppCli.java
+++ speech2text/src/main/java/org/lispeak/speech2text/AppCli.java
@@ -1,38 +1,33 @@
 package org.lispeak.speech2text;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintStream;
 
 import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.LiveSpeechRecognizer;
 import edu.cmu.sphinx.api.SpeechResult;
-import edu.cmu.sphinx.api.StreamSpeechRecognizer;
 
 /**
  * @see https://cmusphinx.github.io/wiki/tutorialsphinx4/#using-sphinx4-in-your-projects
  *
  */
-public class AppCli {
+public class AppCli extends LiveSpeechRecognizer {
 
 	public final static String LANG_DEFAULT = "it-IT";
-	
+
 	public static void main(String[] args) throws Exception {
 
 		// TODO some kind of notification to user???
 		// TODO how to handle languages?
 		// TODO how to trap signals?
-		
-		new AppCli();
-		
+		AppCli app = new AppCli();
+		app.mainLoop(System.out);
 	}
 
 	public AppCli() throws IOException {
-		Configuration configuration = getConfiguration(LANG_DEFAULT);
-
-		microphoneToStream(configuration, System.out);
+		super(getConfiguration(LANG_DEFAULT));
 	}
-	
+
 	/**
 	 * Create a configuration object, assuming all models are under
 	 * /speech_recognition/data/&lt;lang&gt;
@@ -41,7 +36,7 @@ public class AppCli {
 	 *            e.g. it-IT
 	 * @return
 	 */
-	public Configuration getConfiguration(String lang) {
+	public static Configuration getConfiguration(String lang) {
 		Configuration configuration = new Configuration();
 
 		// configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
@@ -64,14 +59,13 @@ public class AppCli {
 	 * @param os
 	 * @throws IOException
 	 */
-	public void microphoneToStream(Configuration configuration, PrintStream os) throws IOException {
+	public void mainLoop(PrintStream os) throws IOException {
 
-		LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
 		// Start recognition process pruning previously cached data.
-		recognizer.startRecognition(true);
+		startRecognition(true);
 		SpeechResult result;
 		System.err.println("Ready.");
-		while ((result = recognizer.getResult()) != null) {
+		while ((result = getResult()) != null) {
 			os.format("Hypothesis: %s\n", result.getHypothesis());
 			os.format("Nbest: %s\n", result.getNbest(6));
 			// os.format("Words: %s\n", result.getWords());
@@ -79,29 +73,7 @@ public class AppCli {
 		}
 		// Pause recognition process. It can be resumed then with
 		// startRecognition(false).
-		recognizer.stopRecognition();
-	}
-
-	/**
-	 * Read data from audio input stream, print text to output stream
-	 * 
-	 * @param configuration
-	 * @param is
-	 * @param os
-	 * @throws IOException
-	 */
-	public void streamToStream(Configuration configuration, InputStream is, PrintStream os) throws IOException {
-
-		StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(configuration);
-		// Start recognition process pruning previously cached data.
-		recognizer.startRecognition(is);
-		SpeechResult result;
-		while ((result = recognizer.getResult()) != null) {
-			os.format("Hypothesis: %s\n", result.getHypothesis());
-		}
-		// Pause recognition process. It can be resumed then with
-		// startRecognition(false).
-		recognizer.stopRecognition();
+		stopRecognition();
 	}
 
 }
--- speech2text/src/main/java/org/lispeak/speech2text/AppGui.java
+++ speech2text/src/main/java/org/lispeak/speech2text/AppGui.java
@@ -7,6 +7,7 @@ import java.awt.Menu;
 import java.awt.MenuItem;
 import java.awt.PopupMenu;
 import java.awt.SystemTray;
+import java.awt.Toolkit;
 import java.awt.TrayIcon;
 import java.awt.TrayIcon.MessageType;
 import java.io.IOException;
@@ -23,93 +24,88 @@ public class AppGui extends AppCli {
 	public final static String LANG_DEFAULT = "it-IT";
 
 	TrayIcon trayIcon;
-	
+
 	public static void main(String[] args) throws Exception {
 
 		// TODO some kind of notification to user???
 		// TODO how to handle languages?
 		// TODO how to trap signals?
-		
-		new AppGui();
-		
+
+		AppGui app = new AppGui();
+		app.mainLoop(System.out);
+
 	}
 
 	public AppGui() throws IOException {
 		// recognition already started here
 		showIconInSystemTray();
 	}
-	
-	
+
 	public void showIconInSystemTray() {
 		if (!SystemTray.isSupported()) {
-            System.err.println("SystemTray is not supported");
-            return;
-        }
-        final PopupMenu popup = new PopupMenu();
-        trayIcon =
-                new TrayIcon(createImage("mic.png", "tray icon"));
-/* can try these:
-        Image image = Toolkit.getDefaultToolkit().createImage("icon.png");
-        //Alternative (if the icon is on the classpath):
-        //Image image = Toolkit.getToolkit().createImage(getClass().getResource("icon.png"));
-*/
-        final SystemTray tray = SystemTray.getSystemTray();
-       
-        // Create a pop-up menu components
-        MenuItem aboutItem = new MenuItem("About");
-        CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
-        CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
-        Menu displayMenu = new Menu("Display");
-        MenuItem errorItem = new MenuItem("Error");
-        MenuItem warningItem = new MenuItem("Warning");
-        MenuItem infoItem = new MenuItem("Info");
-        MenuItem noneItem = new MenuItem("None");
-        MenuItem exitItem = new MenuItem("Exit");
-       
-        //Add components to pop-up menu
-        popup.add(aboutItem);
-        popup.addSeparator();
-        popup.add(cb1);
-        popup.add(cb2);
-        popup.addSeparator();
-        popup.add(displayMenu);
-        displayMenu.add(errorItem);
-        displayMenu.add(warningItem);
-        displayMenu.add(infoItem);
-        displayMenu.add(noneItem);
-        popup.add(exitItem);
-       
-        trayIcon.setPopupMenu(popup);
-       
-        try {
-            tray.add(trayIcon);
-        } catch (AWTException e) {
-            System.err.println("TrayIcon could not be added.");
-        }
+			System.err.println("SystemTray is not supported");
+			return;
+		}
+		final PopupMenu popup = new PopupMenu();
+		Image image = createImage("/listen.png","system tray");
+				//Toolkit.getDefaultToolkit().createImage(getClass().getResource("listen.png"));
+		trayIcon = new TrayIcon(image);
+		final SystemTray tray = SystemTray.getSystemTray();
+
+		// Create a pop-up menu components
+		MenuItem aboutItem = new MenuItem("About");
+		CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
+		CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
+		Menu displayMenu = new Menu("Display");
+		MenuItem errorItem = new MenuItem("Error");
+		MenuItem warningItem = new MenuItem("Warning");
+		MenuItem infoItem = new MenuItem("Info");
+		MenuItem noneItem = new MenuItem("None");
+		MenuItem exitItem = new MenuItem("Exit");
+
+		// Add components to pop-up menu
+		popup.add(aboutItem);
+		popup.addSeparator();
+		popup.add(cb1);
+		popup.add(cb2);
+		popup.addSeparator();
+		popup.add(displayMenu);
+		displayMenu.add(errorItem);
+		displayMenu.add(warningItem);
+		displayMenu.add(infoItem);
+		displayMenu.add(noneItem);
+		popup.add(exitItem);
+
+		trayIcon.setPopupMenu(popup);
+
+		try {
+			tray.add(trayIcon);
+		} catch (AWTException e) {
+			System.err.println("TrayIcon could not be added.");
+		}
 	}
 
-     
-    //Obtain the image URL
-	//This code from Oracle should be avoided
-    protected Image createImage(String path, String description) {
-        URL imageURL = this.getClass().getResource(path);
-         
-        if (imageURL == null) {
-            System.err.println("Resource not found: " + path);
-            return null;
-        } else {
-            return (new ImageIcon(imageURL, description)).getImage();
-        }
-    }
-	
+	// Obtain the image URL
+	// This code from Oracle should be avoided
+	protected Image createImage(String path, String description) {
+		URL imageURL = this.getClass().getResource(path);
+
+		if (imageURL == null) {
+			System.err.println("Resource not found: " + path);
+			return null;
+		} else {
+			return (new ImageIcon(imageURL, description)).getImage();
+		}
+	}
+
 	public void showNotification(String title, String msg, MessageType type) {
 		trayIcon.displayMessage(title, msg, type);
 	}
-	
+
 	public void showNotification(String title, String msg) {
 		showNotification(title, msg, MessageType.INFO);
 	}
-	
+
 	public void showNotification(String title) {
 		showNotification(title, "", MessageType.INFO);
 	}
