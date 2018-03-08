32c497a80cf0ec8ee64582d96533c922e653260f
Luca Vercelli <luca.vercelli.to@gmail.com>
2018-03-08 22:25:39 +0100

    ini file

--- speech2text/src/main/java/org/lispeak/speech2text/AppCli.java
+++ speech2text/src/main/java/org/lispeak/speech2text/AppCli.java
@@ -1,7 +1,13 @@
 package org.lispeak.speech2text;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.prefs.Preferences;
+
+import org.ini4j.Ini;
+import org.ini4j.IniPreferences;
+import org.ini4j.InvalidFileFormatException;
 
 import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.LiveSpeechRecognizer;
@@ -13,7 +19,9 @@ import edu.cmu.sphinx.api.SpeechResult;
  */
 public class AppCli extends LiveSpeechRecognizer {
 
-	public final static String LANG_DEFAULT = "it-IT";
+	public final static String CONFIG_FILE = System.getProperty("user.home") + File.separator + ".lispeak";
+	public final static String CONFIG_SECTION1 = "General";
+	public final static String LANG_DEFAULT = "it-IT"; // should be en-EN...
 
 	public static void main(String[] args) throws Exception {
 
@@ -25,7 +33,7 @@ public class AppCli extends LiveSpeechRecognizer {
 	}
 
 	public AppCli() throws IOException {
-		super(getConfiguration(LANG_DEFAULT));
+		super(getConfiguration(getLanguage()));
 	}
 
 	/**
@@ -76,4 +84,34 @@ public class AppCli extends LiveSpeechRecognizer {
 		stopRecognition();
 	}
 
+	/**
+	 * Read language from configuration file
+	 * @return
+	 */
+	public static String getLanguage() {
+		Preferences fileini = null;
+		try {
+			fileini = getIniFile();
+		} catch (InvalidFileFormatException e) {
+			System.err.println("Invalid configuration file. It will be ignored.");
+		} catch (IOException e) {
+			System.err.println("I/O error reading configuration file. It will be ignored.");
+		}
+		String lang = (fileini != null) ? fileini.node(CONFIG_SECTION1).get("lang", LANG_DEFAULT) : null;
+		return lang;
+	}
+
+	/**
+	 * Read the whole configuration file.
+	 * @return
+	 * @throws InvalidFileFormatException
+	 * @throws IOException
+	 */
+	public static Preferences getIniFile() throws InvalidFileFormatException, IOException {
+		File fileini = new File(CONFIG_FILE);
+		if (!fileini.exists())
+			return null;
+		Ini ini = new Ini(fileini);
+		return new IniPreferences(ini);
+	}
 }
